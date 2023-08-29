0c95917b24b9d01ac917ebe34266a71e7ea50774
Brandon Asher <basher11700@gmail.com>
2023-08-29 13:24:49 -0400

    Updated Lab

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, totalExpenses } = useContext(AppContext);
+    const [cost, setCost] = useState('');
+
+    if(cost > 20000){
+        alert("The budget cannot exceed 20000");
+        setCost("");
+        return;
+    }
+
+    if(cost < totalExpenses){
+        alert("The budget cannot be hgigher than the spent amount"+ totalExpenses)
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        placeholder = { budget }
+                        value={cost}
+                        step = '10'
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setCost(event.target.value)}>
+                        </input>
         </div>
     );
 };
